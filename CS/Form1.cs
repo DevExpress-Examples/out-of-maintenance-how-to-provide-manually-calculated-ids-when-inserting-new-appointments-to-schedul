09f4ca3d4279b77db5fd18936fad98a0eac1b3d7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using DevExpress.XtraScheduler;
+
+namespace SchedulerAptIdGeneration {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
+            this.carsTableAdapter.Fill(this.carsDBDataSet.Cars);
+            // TODO: This line of code loads data into the 'carsDBDataSet.CarScheduling' table. You can move, or remove it, as needed.
+            this.carSchedulingTableAdapter.Fill(this.carsDBDataSet.CarScheduling);
+
+            this.carsDBDataSet.CarScheduling.TableNewRow += 
+                new DataTableNewRowEventHandler(CarScheduling_TableNewRow);
+
+            schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.WorkWeek;
+            schedulerControl1.Start = schedulerStorage1.Appointments[0].Start;
+        }
+
+        private void OnApptChangedInsertedDeleted(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e) {
+            this.carSchedulingTableAdapter.Update(this.carsDBDataSet);
+            UpdateGridSelection();
+        }
+
+        void CarScheduling_TableNewRow(object sender, DataTableNewRowEventArgs e) {
+            e.Row["ID"] = GenerateNewAppointmentId();
+        }
+
+        private int GenerateNewAppointmentId() {
+            int freeId = 0;
+
+            for (int i = 0; i < schedulerControl1.Storage.Appointments.Count; i++) {
+                Appointment apt = schedulerControl1.Storage.Appointments[i];
+
+                int aptId = Convert.ToInt32(apt.GetValue(schedulerControl1.Storage, "ID"));
+
+                if (aptId > freeId)
+                    freeId = aptId;
+
+                if (apt.Type == AppointmentType.Pattern) {
+                    AppointmentBaseCollection exceptions = apt.GetExceptions();
+
+                    for (int j = 0; j < exceptions.Count; j++) {
+                        aptId = Convert.ToInt32(exceptions[j].GetValue(schedulerControl1.Storage, "ID"));
+
+                        if (aptId > freeId)
+                            freeId = aptId;
+                    }
+                }
+            }
+
+            freeId++;
+            return freeId;
+        }
+
+        private void UpdateGridSelection() {
+            dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.RowCount - 1;
+            dataGridView1.Rows[dataGridView1.RowCount - 1].Selected = true;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to provide manually calculated IDs when inserting new appointments to SchedulerControl
+
+
+<p>Usually the ID column of the database table for appointments is defined as an Identity Column. This means that when a new data row is inserted, its ID column value is calculated automatically (i. e. you should not be concerned about maintaining the correct value for this column). In some scenarios the existing database schema does not allow you to use an Identity Column and you should generate and insert appropriate IDs manually. To do this, first, specify the ID in the TableAdapter.InsertCommand of the TableAdapter that is used to retrieve appointments (CarSchedulingTableAdapter in this example). Then, handle the <a href="http://msdn.microsoft.com/en-us/library/system.data.datatable.tablenewrow.aspx"><u>DataTable.TableNewRow Event</u></a> of DataTable with appointments (CarsScheduling in this example) to assign manually calculated ID to the e.Row["ID"] parameter in this event handler. This should be sufficient in inserting new appointment rows with correct IDs into the database with a corresponding schema.</p><p>Here is the example of a similar scenario, but in the ASP.NET environment:<br />
+<a href="https://www.devexpress.com/Support/Center/p/E2657">How to provide manually calculated IDs when inserting new appointments to ASPxScheduler bound to the SQL Server database</a></p>
+
+<br/>
+
+
